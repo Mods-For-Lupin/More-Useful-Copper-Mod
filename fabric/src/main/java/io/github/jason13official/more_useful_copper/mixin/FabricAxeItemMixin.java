57f8dd07266bd039708558d3c13a21f9fb3121cd
jason13official <jason13official@gmail.com>
2026-03-07 00:16:39 -0500

    scrape wax with axes

--- fabric/src/main/java/io/github/jason13official/more_useful_copper/mixin/FabricAxeItemMixin.java
+++ fabric/src/main/java/io/github/jason13official/more_useful_copper/mixin/FabricAxeItemMixin.java
@@ -1,5 +1,6 @@
 package io.github.jason13official.more_useful_copper.mixin;
 
+import io.github.jason13official.more_useful_copper.api.common.block.IOxidizableBlock;
 import io.github.jason13official.more_useful_copper.api.common.block.WaxableRegistry;
 import java.util.Optional;
 import net.minecraft.advancements.CriteriaTriggers;
@@ -34,10 +35,10 @@ public class FabricAxeItemMixin {
     Optional<BlockState> unwaxedState = Optional.ofNullable(WaxableRegistry.WAX_OFF_BY_BLOCK.get(blockState.getBlock()))
         .map(block -> block.withPropertiesOf(blockState));
 
-    if (unwaxedState.isPresent()) {
-      Player player = context.getPlayer();
-      ItemStack itemStack = context.getItemInHand();
+    Player player = context.getPlayer();
+    ItemStack itemStack = context.getItemInHand();
 
+    if (unwaxedState.isPresent()) {
       if (!level.isClientSide) {
         if (player instanceof ServerPlayer) {
           CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer) player, blockPos, itemStack);
@@ -51,7 +52,25 @@ public class FabricAxeItemMixin {
           itemStack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(context.getHand()));
         }
       }
+      cir.setReturnValue(InteractionResult.sidedSuccess(level.isClientSide));
+      return;
+    }
 
+    Optional<BlockState> scrapedState = IOxidizableBlock.getPrevious(blockState);
+    if (scrapedState.isPresent()) {
+      if (!level.isClientSide) {
+        if (player instanceof ServerPlayer) {
+          CriteriaTriggers.ITEM_USED_ON_BLOCK.trigger((ServerPlayer) player, blockPos, itemStack);
+        }
+        BlockState result = scrapedState.get();
+        level.playSound(null, blockPos, SoundEvents.AXE_SCRAPE, SoundSource.BLOCKS, 1.0F, 1.0F);
+        level.levelEvent(null, LevelEvent.PARTICLES_SCRAPE, blockPos, 0);
+        level.setBlock(blockPos, result, Block.UPDATE_ALL_IMMEDIATE);
+        level.gameEvent(GameEvent.BLOCK_CHANGE, blockPos, GameEvent.Context.of(player, result));
+        if (player != null) {
+          itemStack.hurtAndBreak(1, player, p -> p.broadcastBreakEvent(context.getHand()));
+        }
+      }
       cir.setReturnValue(InteractionResult.sidedSuccess(level.isClientSide));
     }
   }
