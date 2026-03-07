0b2fb3d28e0737b35ee4cc60002d01ee768caf9c
jason13official <jason13official@gmail.com>
2026-03-07 11:48:35 -0500

    update comparator textures/models/render

--- fabric/src/main/java/io/github/jason13official/more_useful_copper/MoreUsefulCopperClientFabric.java
+++ fabric/src/main/java/io/github/jason13official/more_useful_copper/MoreUsefulCopperClientFabric.java
@@ -1,6 +1,10 @@
 package io.github.jason13official.more_useful_copper;
 
+import io.github.jason13official.more_useful_copper.impl.common.registry.ModBlocks;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.blockrenderlayer.v1.BlockRenderLayerMap;
+import net.minecraft.client.renderer.RenderType;
+import net.minecraft.client.renderer.entity.layers.RenderLayer;
 
 public class MoreUsefulCopperClientFabric implements ClientModInitializer {
 
@@ -8,5 +12,15 @@ public class MoreUsefulCopperClientFabric implements ClientModInitializer {
   public void onInitializeClient() {
 
     MoreUsefulCopperClient.init();
+
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.COPPER_COMPARATOR, RenderType.cutout());
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.EXPOSED_COPPER_COMPARATOR, RenderType.cutout());
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WEATHERED_COPPER_COMPARATOR, RenderType.cutout());
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.OXIDIZED_COPPER_COMPARATOR, RenderType.cutout());
+
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WAXED_COPPER_COMPARATOR, RenderType.cutout());
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WAXED_EXPOSED_COPPER_COMPARATOR, RenderType.cutout());
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WAXED_WEATHERED_COPPER_COMPARATOR, RenderType.cutout());
+    BlockRenderLayerMap.INSTANCE.putBlock(ModBlocks.WAXED_OXIDIZED_COPPER_COMPARATOR, RenderType.cutout());
   }
 }
