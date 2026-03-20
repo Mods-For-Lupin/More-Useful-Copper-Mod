9da31755e5efacf775aa472259c6cfe0c6a6e537
jason13official <jason13official@gmail.com>
2026-03-20 00:36:14 -0400

    golem spawning and shears interaction

---
+++ fabric/src/main/java/io/github/jason13official/more_useful_copper/mixin/FabricSheepMixin.java
@@ -0,0 +1,21 @@
+package io.github.jason13official.more_useful_copper.mixin;
+
+import io.github.jason13official.more_useful_copper.impl.common.util.CommonSheepMixinLogic;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.player.Player;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(Sheep.class)
+public class FabricSheepMixin {
+
+  @Inject(at = @At("HEAD"), method = "mobInteract")
+  private void muc$mobInteract(Player player, InteractionHand hand, CallbackInfoReturnable<InteractionResult> cir) {
+    Sheep self = (Sheep) (Object) this;
+    CommonSheepMixinLogic.injectedMobInteract(self, player, hand, cir);
+  }
+}
